26925ff799d7c8547285e3cb8f35a2ca01632481
RuslanZubrin <error.blin@gmail.com>
2025-08-15 02:53:00 +0300

    aw

--- config/urls.py
+++ config/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("", include("fly.urls", namespace="fly")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
